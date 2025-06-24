7d6b573565f7826174a86d2eb2f05501261794f0
Clurio <claudiohcx@gmail.com>
2025-06-24 15:53:37 +0100

    Initial Commit

---
+++ PokemonTcg/PokemonTcgApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTcgApp.swift
+//  PokemonTcg
+//
+//  Created by Cláudio Costa on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTcgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PokemonTcgTests/PokemonTcgTests.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTcgTests.swift
+//  PokemonTcgTests
+//
+//  Created by Cláudio Costa on 24/06/2025.
+//
+
+import Testing
+@testable import PokemonTcg
+
+struct PokemonTcgTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
