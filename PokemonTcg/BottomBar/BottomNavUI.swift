322afd9637243a0d16664f488a1f414d51514513
Clurio <claudiohcx@gmail.com>
2025-06-24 16:51:54 +0100

    Add TabView navigation with Home, Favourites, and Settings views

---
+++ PokemonTcg/BottomBar/BottomNavUI.swift
@@ -0,0 +1,32 @@
+//
+//  BottomNavUI.swift
+//  PokemonTcg
+//
+//  Created by Cláudio Costa on 24/06/2025.
+//
+
+import SwiftUI
+
+struct BottomNavUI: View {
+    var body: some View {
+        TabView{
+            ListUIView()
+                .tabItem{
+                    Label("",systemImage:"house.fill")
+                }
+            FavouriteUIView()
+                .tabItem{
+                    Label("",systemImage:"heart.fill")
+                }
+            
+            SettingsUIView()
+                .tabItem{
+                    Label("",systemImage:"gearshape.fill")
+                }
+        }
+    }
+}
+
+#Preview {
+    BottomNavUI()
+}
--- PokemonTcg/Favourites/FavouriteUIView.swift
+++ PokemonTcg/Favourites/FavouriteUIView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  FavouriteUIView.swift
 //  PokemonTcg
 //
 //  Created by Cláudio Costa on 24/06/2025.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FavouriteUIView: View {
     var body: some View {
-        BottomNavUI()
+        Text("Favourite")
     }
 }
 
 #Preview {
-    ContentView()
+    FavouriteUIView()
 }
