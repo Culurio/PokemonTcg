98b8a60b2ba072cae45b42ba632f896bb83f6d65
Clurio <claudiohcx@gmail.com>
2025-06-27 17:01:22 +0100

    Created the details dialog and added a click behaviour

--- PokemonTcg/Home/ListUIView.swift
+++ PokemonTcg/Home/ListUIView.swift
@@ -9,31 +9,50 @@ import SwiftUI
 
 struct ListUIView: View {
     @State private var query = ""
+    @State private var selectedPokemon: PokemonCard? = nil
+
     let columns = [
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
 
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 0) {
-                SearchView(query: $query)
+        ZStack {
+            NavigationStack {
+                VStack(spacing: 0) {
+                    SearchView(query: $query)
 
-                ScrollView {
-                    LazyVGrid(columns: columns, spacing: 45) {
-                        ForEach(0..<20) { number in
-                            CardUIView(pokemonName: "Charizard")
+                    ScrollView {
+                        LazyVGrid(columns: columns, spacing: 45) {
+                            ForEach(0..<20, id: \.self) { number in
+                                let pokemon = PokemonCard(
+                                    name: "Charizard \(number)",
+                                    type: [.fire],
+                                    rarity: .rare
+                                )
+
+                                CardUIView(pokemon: pokemon) {
+                                    selectedPokemon = pokemon
+                                }
+                            }
                         }
+                        .padding()
                     }
-                    .padding()
                 }
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .navigationTitle("Pokémon")
             }
-            .navigationTitle("Pokémon")
+
+            if let selected = selectedPokemon {
+                PokemonModalOverlayView(pokemon: selected) {
+                    selectedPokemon = nil
+                }
+            }
         }
     }
 }
 
+
+
 #Preview {
-  ListUIView()
+    ListUIView()
 }
