b4c67bca6b408653aca1c8f37c409be09204cec6
Clurio <claudiohcx@gmail.com>
2025-07-17 17:20:54 +0100

    Add load animation and fix retry button

--- PokemonTcg/Home/ListUIView.swift
+++ PokemonTcg/Home/ListUIView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ListUIView: View {
+    @State private var rotationAngle: Double = 0
     @State private var query = ""
     @State var filterType: ElementType? = nil
     @State var filterRarity: Rarity? = nil
@@ -99,16 +100,32 @@ struct ListUIView: View {
     }
 
     private var loadingView: some View {
-        ProgressView()
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        VStack {
+            Image("Pokeball")
+                .resizable()
+                .frame(width: 60, height: 60)
+                .rotationEffect(Angle(degrees: rotationAngle))
+                .onAppear {
+                    withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                        rotationAngle = 360
+                    }
+                }
+                .onDisappear {
+                    rotationAngle = 0
+                }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     }
 
     private func errorView(message: String) -> some View {
         VStack(spacing: 12) {
+            Image("ErrorImage")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
             Text(message)
                 .foregroundColor(.red)
             Button("Retry"){
-                viewModel.loadPokemons()
+                reloadWithFilters()
             }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
